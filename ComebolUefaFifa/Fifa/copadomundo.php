6bfd270799d08fce75688c68b024f1ce84ad7e75
Leo <leotcort@gmail.com>
2023-12-27 14:32:48 -0300

    Atualizacao copa do mundo

--- ComebolUefaFifa/Fifa/copadomundo.php
+++ ComebolUefaFifa/Fifa/copadomundo.php
@@ -1,11 +1,53 @@
+<?php include ('../../BancoDados/conexao.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/cssFifa/copadomundo.css">
+    <title>Copa do mundo</title>
 </head>
 <body>
-    COPA DO MUNDO
+    <div>
+        <h2 class="tituloStyle">Maiores Vencedores</h2>
+
+        <?php
+            $sqlMaioresVencedores = "SELECT * FROM copa_mundo ORDER BY quant_titulos DESC";
+            $result = $conn->query($sqlMaioresVencedores);
+        ?>
+
+        <div class="m-5">
+            <table class="table tabela table-bg">
+                <thead>
+                    <tr>
+                    <th scope="col">Seleção</th>
+                    <th scope="col">Quantidade Titulos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        while($dados = mysqli_fetch_assoc($result)){
+                            echo "<tr>";
+                            echo "<td>". $dados['nome_pais']. "</td>";
+                            echo "<td>". $dados['quant_titulos']. "</td>";
+                            echo "<tr>";
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        
+        <div class="divButtonStyle">
+            <a href="./inicialFifa.html" class="styleAbutton">
+                <button class="btn"><img src="../../img/incon_voltar_white.png" class="rounded" alt="" width="25px"><div class="espacamento"></div>VOLTAR</button>
+            </a>   
+            <a href="./Graficos/graficoCopaDoMundo.php" class="styleAbutton">
+                <button class="btn"><img src="../../img/grafico.png" class="rounded" alt="" width="27px"><div class="espacamento"></div>Grafico</button>
+            </a>            
+        </div>
+
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
